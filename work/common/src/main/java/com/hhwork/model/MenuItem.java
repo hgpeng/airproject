7e8fde03d4adf7c59a7ea41c1a7239a4cb73e581
hgpeng <kkhgp@126.com>
2014-10-28 13:06:40 +0800

    实体

---
+++ work/common/src/main/java/com/hhwork/model/MenuItem.java
@@ -0,0 +1,78 @@
+package com.hhwork.model;
+
+import java.util.Date;
+
+public class MenuItem {
+
+	private int id;
+	
+	private int parentId;
+	
+	private String name;
+	
+	private String url;
+	
+	private String desc;
+	
+	private Date createTime;
+	
+	private String createMan;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(int parentId) {
+		this.parentId = parentId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getCreateMan() {
+		return createMan;
+	}
+
+	public void setCreateMan(String createMan) {
+		this.createMan = createMan;
+	}
+	
+	
+}
