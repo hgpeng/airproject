2d60b7129079f0b0f313b1dcfb77f247844c8269
yellowwind <hwssz@126.com>
2014-11-09 00:09:49 +0800

    22

--- work/airback/web/js/custom/base/saveBaseDataDialog.js
+++ work/airback/web/js/custom/base/saveBaseDataDialog.js
@@ -8,6 +8,7 @@ var saveBaseDataDialog=function(){
                 fields: [
                 { name: "id", type: "hidden" },
                 { display: "名称", name: "name", newline: true, type: "text", validate: { required: true,minlength:3 }, group: "基础信息", groupicon: groupicon},
+                { display: "英文", name: "name_en", newline: true, type: "text", validate: { required: true,minlength:3 }},       
                 { display: "图标", name: "icon",  readonly:true,newline: true, type: "text"},
                 { display: "链接", name: "url", newline: true, type: "text", width:300},
                 { display:'基本类型',name:"baseType",type: "select", comboboxName: "baseTypeSelect", options: { url:'/baseData/getAllBaseTypes.jsps',valueFieldID: "baseTypeId",textField:'name' }}
