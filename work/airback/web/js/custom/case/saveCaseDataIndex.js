0a06ce33706f93c95b63c00f35ab74bddf402904
yellowwind <hwssz@126.com>
2014-11-12 00:20:20 +0800

    ss

--- work/airback/web/js/custom/case/saveCaseDataIndex.js
+++ work/airback/web/js/custom/case/saveCaseDataIndex.js
@@ -9,10 +9,11 @@ var saveBaseDataDialog=function(){
                 { name: "id", type: "hidden" },
                 { display: "名称", name: "name", newline: true, type: "text", validate: { required: true,minlength:3 }},
                 { display: "英文名称", name: "name_en", newline: true, type: "text", validate: { required: true,minlength:3 }},           
+                { display: "封面图", name: "photo",  readonly:true,newline: true, type: "text"},
                 { display: "面积", name: "area", newline: true, type: "text", width:300},
                 { display: "地址", name: "addr", newline: true, type: "text", width:300},
                 { display: "时间", name: "buildtime", newline: true, type: "text", width:300},
-                { display:'基本类型',name:"basedata",type: "select", comboboxName: "baseTypeSelect", options: { url:'/baseData/getAllBaseData.jsps?type=3',valueFieldID: "basedataId",textField:'name' }},
+                { display:'基本类型',name:"basedata",type: "select", comboboxName: "baseTypeSelect", options: { url:'/baseData/getAllBaseData.jsps?type=7',valueFieldID: "basedataId",textField:'name' }},
                 { display: "内容", name: "content", newline: true, type: "textarea", width:300}
                 ]
             });
